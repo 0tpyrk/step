856b1c5c7cabd27c178b8f68e39af71b24d0e529
William <ninjo07@gmail.com>
2020-06-15 23:04:12 +0000

    Add comments

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,6 +15,8 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
@@ -37,12 +39,14 @@ public final class FindMeetingQuery {
     List<TimeRange> finalTimes = Arrays.asList();
     finalTimes = new ArrayList<>(finalTimes);
     List<Event> sortedRelevantEvents = new ArrayList<Event>();
-    // TODO sort personRelevantEvents
+    // sort personRelevantEvents
     while (!personRelevantEvents.isEmpty()) {
       Event e = personRelevantEvents.get(0);
       for (int i = 0; i < personRelevantEvents.size(); i++) {
+        // if event starts earlier than the earliest found so far
         if (TimeRange.ORDER_BY_START.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) > 0) {
           e = personRelevantEvents.get(i);
+        // if equal starts, compare ending times
         } else if (TimeRange.ORDER_BY_START.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) == 0) {
           if (TimeRange.ORDER_BY_END.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) > 0) {
             e = personRelevantEvents.get(i);
@@ -53,29 +57,28 @@ public final class FindMeetingQuery {
       personRelevantEvents.remove(e);
     }
 
+    // remove events that are contained within others
     List<Event> toDelete = new ArrayList<Event>();
     for (int i = 0; i < sortedRelevantEvents.size() - 1; i++) {
       if (sortedRelevantEvents.get(i).getWhen().contains(sortedRelevantEvents.get(i+1).getWhen())) {
         toDelete.add(sortedRelevantEvents.get(i+1));
       }
     }
-
     for (Event e : toDelete) {
       sortedRelevantEvents.remove(e);
     }
-    
-    System.out.println("BEginning");
+
     // test if sort works
+    System.out.println("Beginning");
     for (Event e : sortedRelevantEvents) {
         System.out.println(e.getWhen().start());
     }
 
-    // TODO cut events out of personRelevantEvents that have time ranges outside of possiblility
-    // cut overlapping events? merge them?
     int start = TimeRange.START_OF_DAY;
     int end;
     int difference;
     int duration = (int) request.getDuration();
+    // check between events for enough space for the meeting
     for (Event e : sortedRelevantEvents) {
       end = e.getWhen().start();
       System.out.println("Checking: " + start + " - " + end);
@@ -85,6 +88,7 @@ public final class FindMeetingQuery {
       }
       start = e.getWhen().end();
     }
+    // check between end of last event and end of the day
     end = TimeRange.END_OF_DAY;
     difference = end - start;
     if (difference >= duration) finalTimes.add(TimeRange.fromStartEnd(start, end, true));
