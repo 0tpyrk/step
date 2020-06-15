b0a6b618f12ddd01ca4252f99c9270a4d9bd84b2
William <ninjo07@gmail.com>
2020-06-15 18:08:32 +0000

    Complete first version of algorithm that passes tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,80 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // events that have 1 or more attendees of query event attending them
+    List<Event> personRelevantEvents = Arrays.asList();
+    personRelevantEvents = new ArrayList<>(personRelevantEvents);
+    // fill personRelevantEvents
+    for (Event e : events) {
+      for (String p : e.getAttendees()) {
+        if (request.getAttendees().contains(p)) {
+          personRelevantEvents.add(e);
+          break;
+        }
+      }
+    }
+
+    List<TimeRange> finalTimes = Arrays.asList();
+    finalTimes = new ArrayList<>(finalTimes);
+    List<Event> sortedRelevantEvents = new ArrayList<Event>();
+    // TODO sort personRelevantEvents
+    while (!personRelevantEvents.isEmpty()) {
+      Event e = personRelevantEvents.get(0);
+      for (int i = 0; i < personRelevantEvents.size(); i++) {
+        if (TimeRange.ORDER_BY_START.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) > 0) {
+          e = personRelevantEvents.get(i);
+        } else if (TimeRange.ORDER_BY_START.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) == 0) {
+          if (TimeRange.ORDER_BY_END.compare(e.getWhen(), personRelevantEvents.get(i).getWhen()) > 0) {
+            e = personRelevantEvents.get(i);
+          }
+        }
+      }
+      sortedRelevantEvents.add(e);
+      personRelevantEvents.remove(e);
+    }
+
+    List<Event> toDelete = new ArrayList<Event>();
+    for (int i = 0; i < sortedRelevantEvents.size() - 1; i++) {
+      if (sortedRelevantEvents.get(i).getWhen().contains(sortedRelevantEvents.get(i+1).getWhen())) {
+        toDelete.add(sortedRelevantEvents.get(i+1));
+      }
+    }
+
+    for (Event e : toDelete) {
+      sortedRelevantEvents.remove(e);
+    }
+    
+    System.out.println("BEginning");
+    // test if sort works
+    for (Event e : sortedRelevantEvents) {
+        System.out.println(e.getWhen().start());
+    }
+
+    // TODO cut events out of personRelevantEvents that have time ranges outside of possiblility
+    // cut overlapping events? merge them?
+    int start = TimeRange.START_OF_DAY;
+    int end;
+    int difference;
+    int duration = (int) request.getDuration();
+    for (Event e : sortedRelevantEvents) {
+      end = e.getWhen().start();
+      System.out.println("Checking: " + start + " - " + end);
+      difference = end - start;
+      if (difference >= duration) {
+        finalTimes.add(TimeRange.fromStartDuration(start, difference));
+      }
+      start = e.getWhen().end();
+    }
+    end = TimeRange.END_OF_DAY;
+    difference = end - start;
+    if (difference >= duration) finalTimes.add(TimeRange.fromStartEnd(start, end, true));
+    System.out.println(finalTimes);
+    return finalTimes;
   }
 }
